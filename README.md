531645aee21cbf1a79a35473e3c7a9c56ceaa02c
Tyler Kaminski <durcor@disroot.org>
2022-05-23 01:39:58 -0400

    Revamp the command semantics Also add documentation and generalize the purpose

--- README.md
+++ README.md
@@ -1,16 +1,36 @@
-# rspfind
-Recursively search for files with a given permission string in a given directory.
+# rsfind
+Recursively search for files with a variety of things in the given directory.
+
+# Why?
+Too many tools exist to extract simple information from files.
+Solutions like `fgrep` and `find` have archaic syntaxes which don't adhere to traditional human thought when it comes to sifting through files for what you need.
+It is time we provide developers, systems administrators, and power users with the ability to quickly query their files for the information they deserve.
+Let me introduce you to `rsfind` (because it finds info on files, but in Rust).
 
 ## Building
 ```sh
+# Debug build
 cargo build
+# Optimized, symbol-stripped build
+cargo build --release
 ```
 
 ## Usage
 ```sh
-rspfind <directory> <permission string>
+rsfind [permissions, name] [of, with] <query> in <directory>
 ```
 
-## Acceptable Permission Strings
-- rwxrwxrwx
-- rw-r--r--
+## Semantics
+- `name`
+    - of: searches for files with the exact name of the query
+    - with: searched for files with a name containing the substring of the query
+- `permissions`
+    - Some acceptable permission strings
+        - rwxrwxrwx
+        - rw-r--r--
+
+## Some Ideas for the Future
+- fgrep replacement: `rsfind {contents, data} with <query> in <directory>`
+- largest file/directory: `rsfind max in <directory>`
+- files/directories larger than a size: `rsfind size {greater, less} than <query> in <directory>`
+- modification/creation date: `rsfind {newer, older} than <query> in <directory>`
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-// Recursively finds files with a given permission string in a directory.
+// Recursively finds files with given permissions/name in a directory.
 
 use std::{
     env::args,
@@ -8,30 +8,57 @@ use std::{
     path::Path,
 };
 
-fn find(dir: &Path, perms: u32) -> Result<()> {
+// TODO: Should I suppress non-fatal errors?
+fn find_name(dir: &Path, name: &String, exact: bool) -> Result<()> {
     for entry in read_dir(dir)? {
         let path = entry?.path();
         if path.is_dir() && !path.is_symlink() {
-            match find(&path, perms) {
+            match find_name(&path, name, exact) {
                 Ok(_) => (),
-                Err(e) => eprintln!("Warning: {}: '{}'", e, path.display()),
+                Err(_) => (),
             }
-        } else if path.metadata()?.permissions().mode() == perms {
-            println!("{}", path.display());
+        } else if path.is_file() {
+            let cur_name = path.file_name().unwrap().to_str().unwrap();
+            if (exact && cur_name == name) || cur_name.contains(name) {
+                println!("{}", path.display())
+            }
         }
     }
     Ok(())
 }
 
-fn main() -> Result<()> {
-    let args: Vec<String> = args().collect();
+// fn walk_dir(dir: &Path) -> Result<()> {
+//     for entry in read_dir(dir)? {
+//         let path = entry?.path();
+//         if path.is_dir() && !path.is_symlink() {
+//             match walk_dir(&path) {
+//                 Ok(_) => (),
+//                 Err(_) => (),
+//             }
+//         } else if path.is_file() {
+//             println!("{}", path.display())
+//         }
+//     }
+//     Ok(())
+// }
 
-    if args.len() != 3 {
-        println!("Usage: {} <directory> <permissions string>", args[0]);
-        return Ok(());
-    }
-    let path = Path::new(&args[1]).canonicalize()?;
-    let perm_bytes = &args[2].as_bytes();
+// fn find_max<'a>(dir: &Path, max: &u64, max_file: &'a Path) -> Result<&'a Path> {
+//     for entry in read_dir(dir)? {
+//         let path = entry?.path();
+//         if path.is_dir() && !path.is_symlink() {
+//             find_max(&path, max, max_file);
+//         } else if path.is_file() {
+//             let size = path.metadata()?.len();
+//             if size > *max {
+//                 *max = size;
+//                 *max_file = path;
+//             }
+//         }
+//     }
+//     Ok(max_file)
+// }
+
+fn validate_perms(path: &Path, perm_bytes: &str) -> Result<()> {
     // Validate that the given permissions string is valid.
     if perm_bytes.len() != 9 {
         return Err(Error::new(
@@ -41,20 +68,60 @@ fn main() -> Result<()> {
     }
     // Create a bitmask of the permissions.
     let mut perms = 1 << 15;
-    for (i, (p, v)) in perm_bytes
-        .iter()
-        .zip("rwxrwxrwx".as_bytes().iter())
-        .enumerate()
-    {
+    for (i, (p, v)) in perm_bytes.chars().zip("rwxrwxrwx".chars()).enumerate() {
         if p == v {
             perms |= 1 << (8 - i);
-        } else if *p != b'-' {
+        } else if p != '-' {
             return Err(Error::new(
                 InvalidInput,
                 "Permissions string must be of the form: rwxrwxrwx.",
             ));
         }
     }
+    fn find_perms(dir: &Path, perms: u32) -> Result<()> {
+        for entry in read_dir(dir)? {
+            let path = entry?.path();
+            if path.is_dir() && !path.is_symlink() {
+                match find_perms(&path, perms) {
+                    Ok(_) => (),
+                    Err(_) => (),
+                }
+            } else if path.metadata()?.permissions().mode() == perms {
+                println!("{}", path.display())
+            }
+        }
+        Ok(())
+    }
     // Find files at the current path with the requested permissions.
-    Ok(find(&path, perms)?)
+    Ok(find_perms(&path, perms)?)
+}
+
+fn main() -> Result<()> {
+    let args: Vec<String> = args().collect();
+
+    if args.len() != 6 {
+        println!(
+            "Usage: {} [permissions, name] [of, with] <query> in <directory>",
+            args[0]
+        );
+        return Ok(());
+    }
+
+    let path = Path::new(&args[5]).canonicalize()?;
+    match args[1].as_str() {
+        "permissions" => validate_perms(&path, &args[3]),
+        "name" => match args[2].as_str() {
+            "of" => find_name(&path, &args[3], true),
+            "with" => find_name(&path, &args[3], false),
+            _ => Err(Error::new(
+                InvalidInput,
+                "Must specify 'of' or 'with' after 'name'.",
+            )),
+        },
+        // "size" => Ok(println!("{:#?}", find_max(&path, &0, &path))),
+        _ => Err(Error::new(
+            InvalidInput,
+            "Must specify 'permissions' or 'name' before 'of' or 'with'.",
+        )),
+    }
 }
